89a06db623c5714aa1f7ab005b4bbb3d1cd091d8
eribert0 <ierigeek@gmail.com>
2025-10-26 13:34:44 -0300

    feat: Implementa função de extração de dados da API com paginação

--- etl.py
+++ etl.py
@@ -1,5 +1,53 @@
 import requests
 import re
+import json
+import time
+import pandas as pd
+import sqlite3
+
+API_BASE_URL = 'https://api.devmka.online/products'
+
+def extrair_dados():
+    print('Iniciando extração de dados...')
+
+    todos_os_produtos = []
+
+    params={
+        'page': 1,
+        'limit': 12
+    }
+
+    try:
+        resposta = requests.get(API_BASE_URL, params=params)
+        resposta.raise_for_status()
+        data = resposta.json()
+
+        todos_os_produtos.extend(data['data'])
+
+        total_de_paginas = data['meta']['last_page']
+        print(f'total de páginas a coletar: {total_de_paginas}')
+    
+        for pagina in range(2, total_de_paginas+1):
+            print(f'Coletando página {pagina} de {total_de_paginas}')
+            params['page'] = pagina
+
+            resposta_loop = requests.get(API_BASE_URL, params=params)
+            resposta_loop.raise_for_status()
+
+            produtos_da_pagina = resposta_loop.json()['data']
+            todos_os_produtos.extend(produtos_da_pagina)
+
+            time.sleep(0.1)
+
+    except requests.RequestException as e:
+        print(f'Erro ao acessar API na página: {e}')
+        return []
+
+    for produto in todos_os_produtos:
+        produto['product_url'] = f'https://testdata.devmka.online/products/{produto['id']}'
+
+    print(f'Extração concluída. {len(todos_os_produtos)} produtos encontrados.')
+    return todos_os_produtos
 
 def main():
     try:
@@ -7,21 +55,6 @@ def main():
     except Exception as e:
         print(f'')
 
-def get_title(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-
-        match = re.search(r'<title>(.*?)</title>', response.text, re.IGNORECASE|re.DOTALL)
-
-        if match:
-            title = match.group(1).strip()
-            print(f'Título da página: {title}')
-        else:
-            print('Nenhum título encontrado.')
-    except Exception as e:
-        print(f'Erro ao acessar: {e}')
-
 if __name__ == '__main__':
-    site = input(f'Digite a url do site: ')
-    get_title(site)
+    # main()
+    extrair_dados()
