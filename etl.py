f4437033d77455b5e81348e895249ac9c223e372
eribert0 <ierigeek@gmail.com>
2025-10-25 23:31:00 -0300

    chore: cria estrutura inicial do projeto

--- etl.py
+++ etl.py
@@ -3,32 +3,25 @@ import re
 
 def main():
     try:
-        response = requests.get("https://ge.globo.com/")
-        if response.status_code == 200:
-            print("✅ Requisição bem-sucedida!")
-            print("Resposta da API do GitHub:")
-            print(response.json()["current_user_url"])
-        else:
-            print(f"⚠️ Erro na requisição: {response.status_code}")
+        ...
     except Exception as e:
-        print(f"❌ Ocorreu um erro: {e}")
+        print(f'')
 
 def get_title(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
 
-        match = re.search(r"<title>(.*?)</title>", response.text, re.IGNORECASE | re.DOTALL)
+        match = re.search(r'<title>(.*?)</title>', response.text, re.IGNORECASE|re.DOTALL)
 
         if match:
             title = match.group(1).strip()
-            print(f"✅ Título da página: {title}")
+            print(f'Título da página: {title}')
         else:
-            print("⚠️ Nenhum título encontrado.")
+            print('Nenhum título encontrado.')
     except Exception as e:
-        print(f"❌ Erro ao acessar {url}: {e}")
+        print(f'Erro ao acessar: {e}')
 
-if __name__ == "__main__":
-    #main()
+if __name__ == '__main__':
     site = input(f'Digite a url do site: ')
     get_title(site)
